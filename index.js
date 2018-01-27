546e8e356d858e11afc9ae4b494fae22a585cdfe
David Jsa <david@jsa.me>
2018-01-26 22:44:40 -0500

    add eslint and clean code

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
-const net = require('net');
+const net = require('net')
 
-let socket = new net.Socket();
-let dataBuf = Buffer.from([]);
+let socket = new net.Socket()
+let dataBuf = Buffer.from([])
 
 socket.on('connect', function () {
-    console.log("socket.on connect");
-    let request = {
-        command: 'summary'
-    }
-    socket.write(JSON.stringify(request));
-});
+  console.log('socket.on connect')
+  let request = {
+    command: 'summary'
+  }
+  socket.write(JSON.stringify(request))
+})
 
 socket.on('data', function (data) {
-    console.log("socket.on data");
-    dataBuf = Buffer.concat([dataBuf, data]);
-});
+  console.log('socket.on data')
+  dataBuf = Buffer.concat([dataBuf, data])
+})
 
 socket.on('end', function () {
-    console.log("socket.on end");
-    console.log(dataBuf.toString('hex'));
-    let string = dataBuf.toString();
-    let lastchar = string.slice(-1);
-    string = string.substr(0, string.length - 1)
-    console.log(string);
+  console.log('socket.on end')
+  console.log(dataBuf.toString('hex'))
+  let string = dataBuf.toString()
+  // let lastchar = string.slice(-1)
+  string.slice(-1)
+  string = string.substr(0, string.length - 1)
+  console.log(string)
 
-    let dataObj = JSON.parse(string);
-    console.log(dataObj);
-});
+  let dataObj = JSON.parse(string)
+  console.log(dataObj)
+})
 
 socket.on('error', function (error) {
-    console.log("socket.on error");
-    console.log(error);
-});
+  console.log('socket.on error')
+  console.log(error)
+})
 
-socket.on('close', function (had_error) {
-    console.log("socket.on close");
-    console.log(had_error);
-});
+socket.on('close', function (hadError) {
+  console.log('socket.on close')
+  console.log(hadError)
+})
 
 socket.on('timeout', function () {
-    console.log("socket.on timeout");
-});
+  console.log('socket.on timeout')
+})
 
 socket.connect({
-    port: 4028,
-    host: '192.168.1.120'
-});
-
+  port: 4028,
+  host: '192.168.1.120'
+})
